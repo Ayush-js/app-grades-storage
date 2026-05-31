3eb70d7bc28835a2e8bf5a833c02ba61fa1a9eb4
Ayush-js <ayushmishraamlai34@gmail.com>
2026-05-31 20:11:05 +0530

    "testing grade class"

--- src/test/java/com/ltp/gradesubmission/GradeServiceTest.java
+++ src/test/java/com/ltp/gradesubmission/GradeServiceTest.java
@@ -1,8 +1,10 @@
 package com.ltp.gradesubmission;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -27,8 +29,20 @@ public class GradeServiceTest {
     @Test
     public void getGradesFromRepoTest(){
        when(gradeRepository.getGrades()).thenReturn(Arrays.asList(
-        new Grade("Harry", "Potion", "B+")
+        new Grade("Harry", "Potion", "B+"),
+        new Grade("Hermione", "Arithmancy", "A+")
        ));
+
+       List<Grade> result = gradeService.getGrades();
+
+       assertEquals("Harry", result.get(0).getName());
+       assertEquals("Arithmancy", result.get(1).getSubject());
+
+    }
+
+    @Test 
+    public void gradeIndexTest(){
+        
     }
 }
 
