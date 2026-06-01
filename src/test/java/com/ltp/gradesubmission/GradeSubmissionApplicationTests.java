65b44c2085fab4d46e7a0c61b395b7e80fb3676a
Ayush-js <ayushmishraamlai34@gmail.com>
2026-06-01 15:15:37 +0530

    removing controller testing

--- src/test/java/com/ltp/gradesubmission/GradeSubmissionApplicationTests.java
+++ src/test/java/com/ltp/gradesubmission/GradeSubmissionApplicationTests.java
@@ -4,19 +4,26 @@ import static org.junit.Assert.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.ltp.gradesubmission.Controller.GradeController;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GradeSubmissionApplicationTests {
 
+
+
 @Autowired
-private GradeController controller;
+private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
-		assertNotNull(controller);
+		
+		assertNotNull(mockMvc);
+
+
+		
 	}
 
 }
